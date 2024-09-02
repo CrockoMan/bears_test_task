84c058bac710ff18c3f3bd24163499d486fed7b0
Konstantin Gurashkin <crocko@mail.ru>
2024-09-02 19:46:34 +0300

    fix: bot added not found item message

--- bot/bot.py
+++ bot/bot.py
@@ -29,11 +29,9 @@ async def handle_product_request(message: types.Message):
         nm_id = message.text
         response = await get_product_description(nm_id)
 
-        if response is None:
+        if (response is None or (response['status'] != HTTPStatus.NOT_FOUND
+                                 and response['status'] != HTTPStatus.OK)):
             await message.reply(RESPONSE_ERROR)
-            return
-        elif response['status'] == HTTPStatus.NOT_FOUND:
-            await message.reply(NOT_FOUND_ERROR)
             return
         elif response['status'] != HTTPStatus.OK:
             await message.reply(NOT_FOUND_ERROR)
